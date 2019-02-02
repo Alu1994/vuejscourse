a3b4da5ce9c2e2cbe108125b26128ed3c4acee78
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-02-02 17:35:55 -0200

    dynamic components and slots :)

---
+++ Exercises/Exercise_8_Section_9__(DynamicComponents__Slots)/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="changeComponent('Blue')">Load Blue Template</button>
+                <button class="btn btn-success" @click="changeComponent('Green')">Load Green Template</button>
+                <button class="btn btn-danger" @click="changeComponent('Red')">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>This is the default slot!!</p>
+                        <p :slot="slotName">{{ someText }}</p>
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function() {
+            return {
+                someText: "Blue",
+                slotName: "Blue",
+                selectedComponent: "appBlue"
+            };
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        methods: {
+            changeComponent(componentColor) {
+                this.selectedComponent = 'app' + componentColor;
+                this.someText = componentColor;
+                this.slotName = componentColor;
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Exercises/Exercise_8_Section_9__(DynamicComponents__Slots)/src/components/Red.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <slot>
+            
+        </slot>
+        <slot name="Red">
+
+        </slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+        background-color: lightcoral;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
