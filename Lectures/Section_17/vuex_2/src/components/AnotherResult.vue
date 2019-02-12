697c57f87d51e011a990ddd0d4a7b61c92d31958
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2019-02-11 23:54:50 -0200

    vuex to handle values throughout the app

---
+++ Lectures/Section_17/vuex_2/src/components/AnotherResult.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <!-- <p>Counter is: {{ counter }}</p>
+        <p>Number of Clicks: {{ clicks }}</p> -->
+
+        <!-- <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of Clicks: {{ stringCounter }}</p> -->
+        
+        <p>Counter is: {{ doubles }}</p>
+        <p>Number of Clicks: {{ strings }}</p>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+
+    export default {
+        //props: ['counter']
+        
+        // computed: {
+        //     counter() {
+        //         //return this.$store.state.counter * 2;
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         //return this.$store.state.counter * 2;
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+
+        // computed: mapGetters([
+        //     'doubleCounter',
+        //     'stringCounter'
+        // ])
+
+        computed: {
+            // Please pull all the methods and objects inside "mapGetters"
+            // and create separate key value pairs here
+            ...mapGetters({
+                doubles: 'doubleCounter',
+                strings: 'stringCounter'
+            })
+        }
+    }
+</script>
